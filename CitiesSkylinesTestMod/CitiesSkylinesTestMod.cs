d829b7d72e4c6ea8cfe20f19658df8eaf47d13cd
Yasushi Sakai <yasushi.accounts@fastmail.com>
2022-05-25 12:40:56 -0400

    a empty mod

---
+++ CitiesSkylinesTestMod/CitiesSkylinesTestMod.cs
@@ -0,0 +1,12 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace CitiesSkylinesTestMod
+{
+    public class CitiesSkylinesTestMod : IUserMod
+    {
+        public string Name => "Test Mod";
+
+        public string Description => "This is a test mod.";
+    }
+}
